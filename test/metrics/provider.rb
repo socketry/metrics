2dd92a58868f1d63b0b36eb3661f297a65c196f6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-11-10 11:18:28 +1300

    Add external tests. (#4)

--- test/metrics/provider.rb
+++ test/metrics/provider.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2021-2022, by Samuel Williams.
+# Copyright, 2022, by Samuel Williams.
 
 unless ENV['METRICS_BACKEND']
 	abort "No backend specified, tests will fail!"
