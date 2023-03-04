4696d039bc68914e7ce8f309df36646696296bf5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-03-04 13:03:53 +1300

    Improved documentation.

--- bake/metrics/capture.rb
+++ bake/metrics/capture.rb
@@ -3,12 +3,13 @@
 # Released under the MIT License.
 # Copyright, 2023, by Samuel Williams.
 
+# Enable capturing metrics.
 def capture
 	ENV['METRICS_BACKEND'] = 'metrics/backend/capture'
 	require 'metrics'
 end
 
-# Generate a list of metrics using the document backend.
+# Generate a list of metrics that have been captured.
 def list
 	Metrics::Backend::Capture.metrics.sort_by!{|metric| metric.name}
 end
